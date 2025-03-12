74ef4c7b736077cf8ab7ffdb3b75ab5216190730
Sanpena <santos.pena@bsu.edu>
2025-03-12 19:05:30 -0400

    Update ApiExample Class

--- src/main/java/edu/bsu/cs/ApiExample.java
+++ src/main/java/edu/bsu/cs/ApiExample.java
@@ -4,25 +4,25 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ChannelListResponse;
 import com.google.api.services.youtube.model.PlaylistListResponse;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.List;
 
 public class ApiExample {
-    // Your API Key here
-    private static final String API_KEY = "AIzaSyBoizCiqHj1I7MMT6j3Z96sVAw92OygejM";
 
+    private static final String API_KEY = "AIzaSyBoizCiqHj1I7MMT6j3Z96sVAw92OygejM";
     private static final String APPLICATION_NAME = "API code samples";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String CHANNEL_ID = "UCIfAvpeIWGHb0duCkMkmm2Q"; // YouTuber's channel ID
 
-    /**
-     * Build and return an authorized API client service using the API Key.
-     *
-     * @return an authorized API client service
-     * @throws GeneralSecurityException, IOException
-     */
+
+
     public static YouTube getService() throws GeneralSecurityException, IOException {
         final YouTube youtubeService = new YouTube.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, null)
@@ -31,25 +31,41 @@ public class ApiExample {
         return youtubeService;
     }
 
-    /**
-     * Call function to create API service object. Define and
-     * execute API request. Print API response.
-     *
-     * @throws GeneralSecurityException, IOException
-     */
     public static void main(String[] args) throws GeneralSecurityException, IOException {
         YouTube youtubeService = getService();
-        // Define and execute the API request
-        YouTube.Playlists.List request = youtubeService.playlists()
-                .list(Arrays.asList("snippet", "contentDetails"));
 
-        // Replace with your desired channel ID
-        PlaylistListResponse response = request.setChannelId("UC_x5XG1OV2P6uZZ5FSM9Ttw")
-                .setMaxResults(25L)
-                .setKey(API_KEY)  // Set your API key here
+        //Gets Channels past 10 streams
+        YouTube.Search.List searchRequest = youtubeService.search()
+                .list(Arrays.asList("id,snippet"))
+                .setKey(API_KEY)
+                .setChannelId(CHANNEL_ID)
+                .setType(Arrays.asList("video"))
+                .setEventType("completed") //completed live streams
+                .setOrder("date") // Order by date
+                .setMaxResults(10L); // Fetch up to 10 results
+
+        SearchListResponse searchResponse = searchRequest.execute();
+        List<SearchResult> results = searchResponse.getItems();
+
+        System.out.println(results);
+
+
+
+
+        // Gets Channels Information
+        YouTube.Channels.List request2 = youtubeService.channels()
+                .list(Arrays.asList("snippet,contentDetails,statistics"));
+        ChannelListResponse response2 = request2.setKey(API_KEY)
+                .setForHandle("@MrBeast")
                 .execute();
+        System.out.println(response2);
 
-        // Print the response
-        System.out.println(response);
+
+
+
     }
+
+
+
+
 }
