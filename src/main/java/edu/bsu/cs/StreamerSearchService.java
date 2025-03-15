4ed741f9ad6853217c94965b3138e4b99800413a
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2025-03-14 21:14:27 -0400

    updated variables and created a constructor

--- src/main/java/edu/bsu/cs/StreamerSearchService.java
+++ src/main/java/edu/bsu/cs/StreamerSearchService.java
@@ -1,11 +1,26 @@
 package edu.bsu.cs;
 
+import com.github.twitch4j.ITwitchClient;
 import com.github.twitch4j.helix.domain.ChannelSearchResult;
+import com.google.api.services.youtube.YouTube;
+import com.github.twitch4j.helix.domain.ChannelSearchList;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StreamerSearchService {
+
+    private final ITwitchClient twitchClient;
+    private final YouTube youtubeService;
+    private final String twitchAuthToken;
+    private final String youtubeApiKey;
+
+    public StreamerSearchService(ITwitchClient twitchClient, YouTube youtubeService, String twitchAuthToken, String youtubeApiKey) {
+        this.twitchClient = twitchClient;
+        this.youtubeService = youtubeService;
+        this.twitchAuthToken = twitchAuthToken;
+        this.youtubeApiKey = youtubeApiKey;
+    }
 
     //Placeholder for implementing search for a twitch streamer
     public List<String> searchTwitchStreamer(String name) {
