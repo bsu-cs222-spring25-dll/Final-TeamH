98e30fb698775f2ac781cadc2ffced1ac9d8a03d
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2025-03-16 18:13:28 -0400

    added a menu for user input, working on implementing user programs

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -1,26 +1,49 @@
 package edu.bsu.cs;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.ChannelListResponse;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.Scanner;
 
 public class Main {
 
+    private static StreamerSearchService searchService;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            int choice = displayMenu(scanner);
+
+            switch(choice) {
+                case 1 -> searchTwitch(scanner);
+                case 2 -> searchYoutube(scanner);
+                case 3 -> {
+                    System.out.println("Exiting Program...");
+                    scanner.close();
+                    return;
+                }
+                default -> System.out.println("Invalid choice! Please enter 1, 2, or 3.");
+            }
+
+        }
+
+
+    }
+
+    public static int displayMenu(Scanner scanner) {
+
+        System.out.println("\n===== Streamer Search Service =====");
+        System.out.println("1. Search for a Twitch Streamer");
+        System.out.println("2. Search for a Youtuber");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice");
+
+        return scanner.nextInt();
+    }
+
+    private static void searchTwitch(Scanner scanner) {
+
+    }
+
+    private static void searchYoutube(Scanner scanner) {
+
+    }
 }
